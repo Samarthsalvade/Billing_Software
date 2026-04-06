6d83ae2820a59c658485a3526742ea67c2cb7891
Samarthsalvade <133511691+Samarthsalvade@users.noreply.github.com>
2026-04-06 17:07:58 +0530

    Added service worker for offline support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.samarth.billing',
+  appName: 'billing-software',
+  webDir: 'build'
+};
+
+export default config;
--- public/sw.js
+++ public/sw.js
@@ -1,47 +1,85 @@
-const CACHE_NAME = 'billing-app-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/static/css/main.css',
-  '/static/js/main.js'
-];
+// const CACHE_NAME = 'billing-app-v1';
+// const urlsToCache = [
+//   '/',
+//   '/index.html',
+//   '/static/css/main.css',
+//   '/static/js/main.js'
+// ];
 
-// Install service worker
+// // Install service worker
+// self.addEventListener('install', (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then((cache) => {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
+
+// // Fetch from cache
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then((response) => {
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request);
+//       }
+//     )
+//   );
+// });
+
+// // Activate and clean old caches
+// self.addEventListener('activate', (event) => {
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) => {
+//       return Promise.all(
+//         cacheNames.map((cacheName) => {
+//           if (cacheName !== CACHE_NAME) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
+const CACHE_NAME = 'billing-app-v1';
+
+// Install
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(async (cache) => {
+      const files = await fetch('/asset-manifest.json')
+        .then(res => res.json())
+        .then(data => Object.values(data.files));
+
+      return cache.addAll(files);
+    })
   );
 });
 
-// Fetch from cache
+// Fetch
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
 
-// Activate and clean old caches
+// Activate
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
+    caches.keys().then((names) =>
+      Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
           }
         })
-      );
-    })
+      )
+    )
   );
 });
