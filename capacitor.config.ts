be87860ae49c8b97258d0a9b8e75cf2d1686af45
Samarthsalvade <133511691+Samarthsalvade@users.noreply.github.com>
2026-04-07 15:11:37 +0530

    fix: added capacitor filesystem

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.samarth.billing',
   appName: 'billing-software',
-  webDir: 'build'
+  webDir: 'build',
+  ios: {
+    contentInset: 'automatic',
+  },
 };
 
 export default config;
